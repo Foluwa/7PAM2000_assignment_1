4870bc9fb901bbd7a9604eb307822b717e338586
foluwa <moronfoluwaakintola@gmail.com>
2023-11-09 03:29:06 +0000

    I implemented charts and analysis

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# 7PAM2000 Applied Data Science 1
-## Assignment 1: Visualisation
+# 7PAM2000 Applied Data Science 1 (Assignment)
 
+## Instructions
+Your task is to apply three types of visualisation methods (graphs) to extract meaningful
+information. Find below a link list for good sources of data, but you are welcome to find
+other sources. Please check the quality. Some data sets can be quite messy and are better
+avoided in your first assignment.
+1. Produce a line plot showing multiple lines with proper labels and legend. Describe
+what conclusions you can draw from this plot.
+2. Produce graphs using two other visualisation methods. Explain why you picked this
+type of graph and describe what conclusions you can draw. Marks will be awarded
+for plausible choices of graphs. The choice does not need to be the perfect one, but
+at least one type will not a good choice if you visualise exactly the same data three
+times. Note, that we consider it not exactly the same, if you do a line plot of a time
+series and then use a pie chart to visualise relative sizes for a selected time.
 
 
 ### Data Source
---
+++ main.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python3
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.ticker import FuncFormatter
+
+
+def billions_formatter(x, pos):
+    """ Formats the tick label to display values in billions. """
+    return f'{x / 1e9:.2f}B'
+
+
+def draw_pie_chart():
+    """ Draw pie chart """
+    # Increase the image size to 8 inches wide and 8 inches high
+    plt.figure(figsize=(14, 8))
+
+    credit_status = df['Credit Status'].value_counts()
+
+    # Create a pie chart with percentages shown on the chart
+    plt.pie(credit_status, labels=credit_status.index, colors=colors, explode=explode, autopct='%1.1f%%')
+    plt.axis('equal')
+
+    # Add a title to the pie chart
+    plt.title('Pie Chart of Credit Status Distribution')
+
+    plt.legend(labels=credit_status.index, loc="best")
+    # Save the plot as an image
+    plt.savefig('pie_chart.png')
+    return plt.show()
+
+
+def draw_horizontal_bar_chart():
+    """ Draw horizontal bars """
+    # Convert the Series index (keys) to a list
+    keys_list = df['Region'].value_counts().index.tolist()
+
+    # Convert the Series values to a list
+    values_list = df['Region'].value_counts().tolist()
+
+    # Set the figure size
+    plt.figure(figsize=(12, 8))
+
+    # Create a horizontal bar chart
+    plt.barh(keys_list, values_list, color=colors)
+
+    # Add labels to the bars
+    for i in range(len(keys_list)):
+        plt.text(values_list[i], i, f'{values_list[i]}', ha='left', va='center')
+
+    # Add a title and labels to the axes
+    plt.title('Breakdown of loans by regions from 1971 to 2023')
+    plt.xlabel('Number of loans')
+    plt.ylabel('Regions')
+
+    # Create a custom legend with different colored patches
+    legend_labels = keys_list
+    legend_handles = [mpatches.Patch(color=colors[i], label=legend_labels[i]) for i in range(len(legend_labels))]
+    plt.legend(handles=legend_handles, loc="best")
+
+    # Save the plot as an image
+    plt.savefig('horizontal_chart.png')
+
+    # Show the chart
+    return plt.show()
+
+
+def draw_line_chart():
+    """ Draws horizontal line"""
+    countries_count = df['Country'].value_counts()
+    countries = countries_count.index.tolist()
+
+    # Convert 'Board Approval Date' to datetime
+    df['Board Approval Date'] = pd.to_datetime(df['Board Approval Date'])
+
+    # Extract the year from 'Board Approval Date'
+    df['Year'] = df['Board Approval Date'].dt.year
+
+    # Filter the data for rows where the 'Country' is in the list of highest borrowers
+    # and Board Approval Date year is 2000 or later
+    df_filtered = df[(df['Country'].isin(countries[:10])) & (df['Year'] >= 2000)]
+
+    # Create a pivot table to combine data for 'India' and 'Sudan' over the years
+    pivot_table = df_filtered.pivot_table(index='Year', columns='Country', values='Disbursed Amount (US$)',
+                                          aggfunc='sum', fill_value=0)
+
+    # Create the line chart with different colors
+    plt.figure(figsize=(12, 10))
+
+    for i, country in enumerate(countries[:10]):
+        plt.plot(pivot_table.index, pivot_table[country], label=country, color=colors[i])
+
+    # Currency Formatter
+    y_format = FuncFormatter(billions_formatter)
+    plt.gca().yaxis.set_major_formatter(y_format)
+
+    # Set labeling
+    plt.title('Loan Disbursed to the Top 8 borrowers Over Time (Since 2000)')
+    plt.xlabel('Loan Year')
+    plt.ylabel('Disbursed Amount (Billions of US$)')
+
+    # Adds legends and grids
+    plt.legend()
+    plt.grid(True)
+
+    # Save the plot as an image
+    plt.savefig('line_chart.png')
+
+    # Show the chart
+    return plt.show()
+
+
+def grouped_bar_chart():
+    """ Draws a grouped bar chart  """
+    # Extract the specified columns
+    df_data = df[['Region', 'Original Principal Amount (US$)', 'Cancelled Amount (US$)', 'Disbursed Amount (US$)',
+                  'Repaid to IDA (US$)', 'Due to IDA (US$)']]
+
+    # Group data by 'Region' and sum the values for each category
+    grouped_data = df_data.groupby('Region').sum()
+
+    # Set the bar width and positions for the bars
+    bar_width = 0.15
+    bar_positions = range(len(grouped_data))
+
+    # Create a grouped bar chart
+    fig, ax = plt.subplots(figsize=(20, 8))
+
+    # Define the categories and corresponding colors
+    categories = ['Original Principal Amount (US$)', 'Cancelled Amount (US$)', 'Disbursed Amount (US$)',
+                  'Repaid to IDA (US$)', 'Due to IDA (US$)']
+
+    # Create grouped bars for each category
+    for i, category in enumerate(categories):
+        ax.bar([pos + i * bar_width for pos in bar_positions], grouped_data[category], bar_width, label=category,
+               color=colors[i])
+
+    # Set the x-axis labels to be the regions
+    ax.set_xticks([pos + (len(categories) - 1) * bar_width / 2 for pos in bar_positions])
+    ax.set_xticklabels(grouped_data.index)
+
+    # Set labels and title
+    ax.set_xlabel('Regions')
+    ax.set_ylabel('Amount (US$)')
+    ax.set_title('Financial Data by Region')
+
+    # Display the legend
+    ax.legend(title='Category', loc='upper right')
+
+    # Rotate x-axis labels for better readability
+    plt.xticks(rotation=45)
+
+    # Apply the custom formatter to the y-axis
+    plt.gca().yaxis.set_major_formatter(FuncFormatter(billions_formatter))
+    # Save the plot as an image
+    plt.savefig('grouped_chart.png')
+    # Show the plot
+    return plt.show()
+
+
+if __name__ == "__main__":
+    # Load data from CSV file
+    df = pd.read_csv('./IDA_Statement_of_Credits_and_Grants__-_Latest_Available_Snapshot_20231101.csv')
+
+    # Define constant chart colors and spacing
+    colors = ['#191970', '#001CF0', '#0038E2', '#0055D4', '#0071C6', '#008DB8',
+              '#00AAAA', '#00C69C', '#00E28E', '#00FF80']
+    explode = (0, 0, 0, 0.1, 0.1, 0.2, 0.3, 0.4, 0.6, 0.8)
+
+    draw_line_chart()  # draws line chart
+    draw_pie_chart()  # draws pie chart
+    draw_horizontal_bar_chart()  # draws bar chart
+    grouped_bar_chart()  # draws bar chart
